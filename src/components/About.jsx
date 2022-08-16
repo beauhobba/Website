40bc4cc2afe2cb7cd6ca4d72e6c31c9e2dc6d43d
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2022-08-16 21:21:47 +1000

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,19 +18,16 @@ function About() {
           <div class="col-lg-5">
             <h1 class="font-weight-light">About</h1>
             <p>
-              Hi I am Beau Hobba, and I am a mechatronics engineer. I moved to Sydney from rural Young, NSW in 2016. I currently still reside in Sydney, Australia and have recently finished with First Class Honours in Mechatronic Engineering. 
-
+            Hi, I am Beau Hobba, and I am a mechatronics engineer. I moved to Sydney from rural Young, NSW, in 2016. I reside in Sydney, Australia and have recently finished with First Class Honours in Mechatronic Engineering.
               <br></br>
               <br></br>
-              I have a talent for leading teams and creating complex systems to match the project. My passions lie in automation, everday assistive robotics, AI, earth/space exploration, argiculture and in general ethical robotics.
-              I am a firm believer that robotics are a central part of our futures and are fascinated by their ability to assist our everday lives. I wish to use my programming expertise to help develop technology which is yet to be discovered. 
+              I am talented at leading teams and creating complex systems to match the project. My passions lie in automation, everyday assistive robotics, AI, earth/space exploration, agriculture, and ethical robotics. I firmly believe robotics is a central part of our future and am fascinated by their ability to assist our everyday lives. I wish to use my programming expertise to help develop technology yet to be discovered.
               <br></br>
               <br></br>
-              My hobbies include flying my drone (yes that is me trying to fish), making video edits, playing/buying board games, making music (it isn't that good), catching and researching crayfish and mountain biking. 
+              My hobbies include flying my drone (yes, that is me trying to fish), making video edits, playing/buying board games, making music (it isn't that good), catching and researching crayfish and mountain biking.
               <br></br>
               <br></br>
-              Stemming from a enjoyment of catching and researching yabbies when I was younger on the farm dam, I now keep some of my own crayfish in a aquaponics setup. One thing led to another, and I found myself walking up remote waterfalls looking for a spiny crayfish. In my spare time I have furthured this joy, into trying to photograph and observe nearly every creature endemic to Australia.
-            </p>
+              Stemming from the enjoyment of catching and researching yabbies when I was younger on the farm dam, I now keep some of my crayfish in an aquaponics setup. One thing led to another, and I found myself walking up remote waterfalls looking for spiny crays. In my spare time, I have furthered this joy by trying to photograph and observe nearly every creature endemic to Australia.  </p>
           </div>
         </div>
       </div>
