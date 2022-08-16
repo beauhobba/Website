220ec9419dfb624c855eca8402f6563c7b899fe3
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2022-08-16 20:01:04 +1000

    removed

--- Website/my-app/src/components/cards/index.js
+++
@@ -1,1 +0,0 @@
-export { default as CardTile } from "./CardTile";
--- Website/my-app/src/components/modals/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Heineken } from "./Heineken";
-export { default as HerbicideMapping } from "./HerbicideMapping";
-export { default as Robotatouille } from "./Robotatouille";
